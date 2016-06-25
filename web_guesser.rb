b2008809ba6c58e7bd7b30b167d85c00146a2191
Trent Whitinger <twhitinger@gmail.com>
2016-06-25 03:58:58 -0600

    added colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,9 +6,17 @@ set :number, rand(101)
 get '/' do
   guess = params["guess"].to_i
   message = game_flow(guess)
-  erb :index, :locals => {:number => settings.number , :message => message}
+  color = background_color(message)
+  erb :index, :locals => {:number => settings.number , :message => message, :color => color }
 end
 
+def background_color(message)
+ if message == "Too high!" || message == "Too low!"
+   "#ff0a12"
+ else
+   "#8cc63f"
+ end
+end
 
 def game_flow(guess)
   if guess > SECRET_NUMBER
